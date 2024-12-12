29886e16456ba4329aeb6f645ad23d207d91d5f6
OzudPCL <ozud.miah@projectcentre.co.uk>
2024-12-12 14:26:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function changeImage(imagePath) {
-    const galleryImage = document.getElementById("galleryImage");
-    const imageLink = document.getElementById("imageLink");
+function changeImage(imageUrl) {
+    const galleryImage = document.getElementById('galleryImage');
+    const imageLink = document.getElementById('imageLink');
+    
+    // Update the image source
+    galleryImage.src = imageUrl;
 
-    galleryImage.src = imagePath;
-    imageLink.href = imagePath;
+    // Update the href of the link to point to the corresponding PDF
+    const pdfUrl = imageUrl.replace('.jpg', '.pdf');
+    imageLink.href = pdfUrl;
 }
+
 
 function scrollToBottom() {
     const target = document.getElementById("scrollTarget");
